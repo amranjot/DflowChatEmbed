5d4bea0820e7c2ee344b0d08ae1a9989e779c797
Harinder <amranjot0001@gmail.com>
2024-05-08 14:49:18 +0530

    add sound

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -8,7 +8,11 @@ export const StarterPromptBubble = (props: Props) => (
       data-modal-target="defaultModal"
       data-modal-toggle="defaultModal"
       class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
-      onClick={() => props.onPromptClick?.()}
+      //onClick={() => props.onPromptClick?.()}
+      onClick={() => {
+        playClickSound(); // Call playClickSound function before invoking onPromptClick
+        props.onPromptClick?.();
+      }}
     >
       <span
         class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
@@ -26,9 +30,8 @@ export const StarterPromptBubble = (props: Props) => (
   </>
 );
 
-
-// // Function to play click sound
-// const playClickSound = () => {
-//   const audio = new Audio('/DflowChatEmbed/multi-pop-1-188165.mp3'); // Replace with actual path
-//   audio.play();
-// };
+// Function to play click sound
+const playClickSound = () => {
+  const audio = new Audio('/DflowChatEmbed/multi-pop-1-188165.mp3'); // Replace with actual path
+  audio.play();
+};
