ea8815bc5df892af11af3552c2745954046e2be2
Harinder <amranjot0001@gmail.com>
2024-05-08 14:06:54 +0530

    finally fix staterprompt bubble flex direction to column

--- src/components/bubbles/StarterPromptBubble.tsx
+++ src/components/bubbles/StarterPromptBubble.tsx
@@ -7,13 +7,14 @@ export const StarterPromptBubble = (props: Props) => (
     <div
       data-modal-target="defaultModal"
       data-modal-toggle="defaultModal"
-      class="flex justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
+      class="flex flex-col justify-start items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
       onClick={() => props.onPromptClick?.()}
     >
       <span
         class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
         data-testid="host-bubble"
         style={{
+          "flex-direction": "column",
           width: 'max-content',
           'font-size': '15px',
           'border-radius': '15px',
